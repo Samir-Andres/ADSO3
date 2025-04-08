f9e9f5a0ca453221345f48c98f255cd694a2a65b
APRENDIZ <APRENDIZ@STMAPRRMDFSP612.FormacionSena.RED>
2025-04-08 15:54:27 -0500

    original

--- App_turismo1/src/view/Jvehiculo.java
+++ App_turismo1/src/view/Jvehiculo.java
@@ -36,6 +36,7 @@ public class Jvehiculo extends JFrame {
 	Vehiculos cr = new Vehiculos();
 	private JButton btnNewButton_1;
 	private JButton BTNUPDATE;
+	private JLabel lblNewLabel;
 	
 
 	/**
@@ -59,7 +60,7 @@ public class Jvehiculo extends JFrame {
 	 */
 	public Jvehiculo() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 406);
+		setBounds(100, 100, 450, 474);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -68,72 +69,72 @@ public class Jvehiculo extends JFrame {
 		
 		JLabel lblNewLabel_1 = new JLabel("Matricula");
 		lblNewLabel_1.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_1.setBounds(54, 29, 61, 14);
+		lblNewLabel_1.setBounds(54, 89, 61, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		txtmatricula = new JTextField();
-		txtmatricula.setBounds(29, 54, 86, 20);
+		txtmatricula.setBounds(29, 114, 86, 20);
 		contentPane.add(txtmatricula);
 		txtmatricula.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Marca");
 		lblNewLabel_2.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_2.setBounds(175, 29, 46, 14);
+		lblNewLabel_2.setBounds(175, 89, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		txtmarca = new JTextField();
-		txtmarca.setBounds(162, 54, 86, 20);
+		txtmarca.setBounds(162, 114, 86, 20);
 		contentPane.add(txtmarca);
 		txtmarca.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Puestos");
 		lblNewLabel_3.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_3.setBounds(311, 29, 46, 14);
+		lblNewLabel_3.setBounds(311, 89, 46, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		txtpuestos = new JTextField();
-		txtpuestos.setBounds(295, 54, 86, 20);
+		txtpuestos.setBounds(295, 114, 86, 20);
 		contentPane.add(txtpuestos);
 		txtpuestos.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Modelo");
 		lblNewLabel_4.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_4.setBounds(54, 102, 61, 14);
+		lblNewLabel_4.setBounds(54, 162, 61, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		txtmodelo = new JTextField();
-		txtmodelo.setBounds(29, 127, 86, 20);
+		txtmodelo.setBounds(29, 187, 86, 20);
 		contentPane.add(txtmodelo);
 		txtmodelo.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Numero del motor");
 		lblNewLabel_5.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_5.setBounds(157, 102, 128, 14);
+		lblNewLabel_5.setBounds(157, 162, 128, 14);
 		contentPane.add(lblNewLabel_5);
 		
 		txtnumeromotor = new JTextField();
-		txtnumeromotor.setBounds(162, 127, 86, 20);
+		txtnumeromotor.setBounds(162, 187, 86, 20);
 		contentPane.add(txtnumeromotor);
 		txtnumeromotor.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("Categoria");
 		lblNewLabel_6.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_6.setBounds(311, 102, 70, 14);
+		lblNewLabel_6.setBounds(311, 162, 70, 14);
 		contentPane.add(lblNewLabel_6);
 		
 		txtcategoria = new JTextField();
-		txtcategoria.setBounds(295, 127, 86, 20);
+		txtcategoria.setBounds(295, 187, 86, 20);
 		contentPane.add(txtcategoria);
 		txtcategoria.setColumns(10);
 		
 		txtidtipotransporte = new JTextField();
-		txtidtipotransporte.setBounds(32, 198, 86, 20);
+		txtidtipotransporte.setBounds(32, 258, 86, 20);
 		contentPane.add(txtidtipotransporte);
 		txtidtipotransporte.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("Id tipo de transporte");
 		lblNewLabel_7.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_7.setBounds(30, 173, 115, 14);
+		lblNewLabel_7.setBounds(30, 233, 115, 14);
 		contentPane.add(lblNewLabel_7);
 		
 		btnRegistrar = new JButton("");
@@ -153,12 +154,12 @@ public class Jvehiculo extends JFrame {
 			}
 		});
 		btnRegistrar.setFont(new Font("Serif", Font.BOLD, 13));
-		btnRegistrar.setBounds(29, 244, 54, 57);
+		btnRegistrar.setBounds(29, 315, 54, 57);
 		contentPane.add(btnRegistrar);
 		
 		btnLimpiar = new JButton("Limpiar");
 		btnLimpiar.setFont(new Font("Serif", Font.BOLD, 13));
-		btnLimpiar.setBounds(175, 314, 89, 23);
+		btnLimpiar.setBounds(175, 385, 89, 23);
 		contentPane.add(btnLimpiar);
 		
 		JButton btnNewButton = new JButton("");
@@ -175,18 +176,18 @@ public class Jvehiculo extends JFrame {
 			}
 		});
 		btnNewButton.setIcon(new ImageIcon("C:\\Users\\APRENDIZ\\Documents\\Iconos\\9004743_trash_delete_bin_remove_icon.png"));
-		btnNewButton.setBounds(121, 244, 61, 57);
+		btnNewButton.setBounds(121, 315, 61, 57);
 		contentPane.add(btnNewButton);
 		
 		txtidvehiculo = new JTextField();
 		txtidvehiculo.setText("");
 		txtidvehiculo.setColumns(10);
-		txtidvehiculo.setBounds(164, 198, 86, 20);
+		txtidvehiculo.setBounds(164, 258, 86, 20);
 		contentPane.add(txtidvehiculo);
 		
 		JLabel lblNewLabel_7_1 = new JLabel("ID Vehiculo");
 		lblNewLabel_7_1.setFont(new Font("Serif", Font.BOLD, 13));
-		lblNewLabel_7_1.setBounds(175, 173, 115, 14);
+		lblNewLabel_7_1.setBounds(175, 233, 115, 14);
 		contentPane.add(lblNewLabel_7_1);
 		
 		btnNewButton_1 = new JButton("");
@@ -197,7 +198,7 @@ public class Jvehiculo extends JFrame {
 			}
 		});
 		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\APRENDIZ\\Documents\\Iconos\\3827704_color_lineal_magnifying glass_sale_search_icon.png"));
-		btnNewButton_1.setBounds(229, 244, 59, 57);
+		btnNewButton_1.setBounds(229, 315, 59, 57);
 		contentPane.add(btnNewButton_1);
 		
 		BTNUPDATE = new JButton("");
@@ -212,8 +213,13 @@ public class Jvehiculo extends JFrame {
 			}
 		});
 		BTNUPDATE.setIcon(new ImageIcon("C:\\Users\\APRENDIZ\\Documents\\Iconos\\6071808_folder_progress_task_update_update task progress_icon.png"));
-		BTNUPDATE.setBounds(322, 244, 59, 57);
+		BTNUPDATE.setBounds(322, 315, 59, 57);
 		contentPane.add(BTNUPDATE);
+		
+		lblNewLabel = new JLabel("Información de Vehículos");
+		lblNewLabel.setFont(new Font("Serif", Font.BOLD, 20));
+		lblNewLabel.setBounds(110, 23, 260, 42);
+		contentPane.add(lblNewLabel);
 		btnLimpiar.addActionListener(e -> clearFields());
 	}
 
